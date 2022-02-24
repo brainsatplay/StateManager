1a0d7d1c9ce379755e64be2182ceaadd46e156a3
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-02-24 03:00:06 -0800

    fix

--- StateManager.js
+++ StateManager.js
@@ -381,7 +381,7 @@ export class StateManager {
                         this.data,
                         "__ANY__",
                         onStateChanged,
-                        interval,
+                        this.interval,
                     );
                 }
                 return this.listener.addFunc("state", onchange);
